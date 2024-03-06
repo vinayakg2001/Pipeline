2c807524018f942f77def4d227d8fc041249675a
Asis Kaur <asis.kaur@theoutsourcepro.com.au>
2024-03-06 12:59:51 +0530

    new page class created

---
+++ src/main/java/Pages/Page.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Pages;
+
+/**
+ * 
+ */
+public class Page {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is on framework branch");
+	}
+
+}
